a908d5b4775724c53d0ecbb6b027eba369eeda66
kamiesu-tomohisa <kamiesu-tomohisa@wiznet.co.jp>
2019-12-26 12:56:46 +0900

    shows bitmaps on ImageView

--- lib/src/utils/vector_utils.dart
+++ lib/src/utils/vector_utils.dart
@@ -6,3 +6,7 @@ Map<String, double> convertVector3ToMap(Vector3 vector) =>
 Map<String, double> convertVector4ToMap(Vector4 vector) => vector == null
     ? null
     : {'x': vector.x, 'y': vector.y, 'z': vector.z, 'w': vector.w};
+
+Vector4 quaternionToVec4 (Quaternion q) {
+  return Vector4(q.x, q.y, q.z, q.w);
+}
